8b59b837ed11e15a0b2aa743ee6fa02e0c0b8b36
lismill <lismill@163.com>
2022-05-16 21:59:46 +0800

    docs: preview

--- docs/assets/index.72551f22.js
+++
@@ -1,1 +0,0 @@
-import{f as n,s as o,o as r,S as t,ai as a}from"./index.a83ca63c.js";const i=n({setup(c){const e=o();return(_,f)=>{const s=a;return r(),t(s,{ref_key:"iframe",ref:e,class:"l-iframe",src:"https://vitejs.dev"},null,512)}}});export{i as default};
