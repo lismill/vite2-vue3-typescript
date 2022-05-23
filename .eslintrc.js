b4949c803c2485d57d8da0e6cf44fe40675e16e4
lismill <lismill@163.com>
2022-05-23 13:23:28 +0800

    fix: 禁止校验变量先定义后使用(为了代码的业务逻辑考虑)

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,8 @@ module.exports = {
     "no-shadow": "off",
     // 禁止使用 v-html
     "vue/no-v-html": "off",
+    // 禁止校验变量先定义后使用(为了代码的业务逻辑考虑)
+    "no-use-before-define": "off",
     // 关闭any类型的警告
     "@typescript-eslint/no-explicit-any": ["off"],
     // 忽略路径别名
