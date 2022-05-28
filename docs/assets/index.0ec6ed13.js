45b12a239545ceef1ad7a95540ea25ca72fba115
lismill <lismill@163.com>
2022-05-28 11:59:04 +0800

    docs: preview

---
+++ docs/assets/index.0ec6ed13.js
@@ -0,0 +1,1 @@
+import{d as f,at as t}from"./index.3f2fd757.js";var e=f({setup(){return()=>t("div",{class:"tsx bg-ffffff p-16"},"\u8FD9\u662F\u4E00\u4E2A .tsx \u6587\u4EF6")}});export{e as default};
