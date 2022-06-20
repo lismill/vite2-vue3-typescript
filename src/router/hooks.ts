7251de787c5c58f384651053d35ee78c7d02025f
lismill <lismill@163.com>
2022-06-20 09:40:33 +0800

    feat: 使用多环境变量

--- src/router/hooks.ts
+++ src/router/hooks.ts
@@ -3,20 +3,20 @@ import Framework from "@/views/_layout/framework.vue";
 export default {
   path: "/hooks",
   component: Framework,
-  redirect: "/hooks/index",
+  redirect: "/hooks/use-env",
   meta: {title: "Hooks", icon: "mdi:dev-to"},
   children: [
-    {
-      path: "index",
-      name: "/hooks/index",
-      component: () => import("@/views/hooks/index.vue"),
-      meta: {title: "Hooks"},
-    },
     {
       path: "use-env",
       name: "/hooks/use-env",
       component: () => import("@/views/hooks/use-env/index.vue"),
-      meta: {title: "当前环境"},
+      meta: {title: "判单当前环境"},
+    },
+    {
+      path: "use-env-value",
+      name: "/hooks/use-env-value",
+      component: () => import("@/views/hooks/use-env-value/index.vue"),
+      meta: {title: "使用环境变量"},
     },
   ],
 };
---
+++ src/views/hooks/use-env-value/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-ffffff p-16">
+    <p v-pre>{{ useEnv() }}</p>
+    :
+    <p>{{ useEnvValue() }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {useEnvValue} from "@/hooks/useEnvValue";
+</script>
+
+<style lang="scss" scoped></style>
